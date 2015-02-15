52d64e5a5c57ab1dde5c874636d2f891ea3d9501
Sridhar Gopinath <g.sridhar53@gmail.com>
2015-02-15 09:39:01 +0530

    Added uploads directory

---
+++ interface/night_out/uploads_corei3/admin/1/1_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b;
+//    char *str = "hello" ;
+    scanf("%d",&n);
+
+  //  str[1] = 'z' ;
+    while(n--)
+    {
+        scanf("%d%d",&a,&b);
+        printf("%d\n",(a - b));
+    }
+//    printf("\n\n\n\n");
+    return 0;
+}
